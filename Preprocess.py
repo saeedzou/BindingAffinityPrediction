5dccdccf6123fed668ffe1b2e87335863e40b13b
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2023-08-23 07:01:50 +0330

    add logging

--- Preprocess.py
+++ Preprocess.py
@@ -2,14 +2,18 @@ import pandas as pd
 import argparse
 
 def split_train_test(csv_path, test_size=0.15, val_size=0.15, train_path="train.csv", val_path="val.csv", test_path="test.csv"):
+    print("-- Reading csv file --")
     df = pd.read_csv(csv_path, index_col=0)
     # Split into train and val and test
     train = df.sample(frac=1-test_size-val_size, random_state=42)
     val = df.drop(train.index).sample(frac=val_size/(test_size+val_size), random_state=42)
     test = df.drop(train.index).drop(val.index)
     # Save to csv
+    print("-- Saving train csv --")
     train.to_csv(train_path)
+    print("-- Saving validation csv --")
     val.to_csv(val_path)
+    print("-- Saving test csv --")
     test.to_csv(test_path)
     return train, val, test
 
